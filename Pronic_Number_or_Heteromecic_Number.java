6b726ffc6202939c4e000c172311753a49da1b16
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-08-24 21:27:36 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int flag=0;         for(int i=1;i<n;i++){             if((i*(i+1))==n){              flag=1;                 break;             }         }         if (flag==1){             System.out.print("YES");         }         else{             System.out.print("NO");         }     } }

---
+++ Pronic_Number_or_Heteromecic_Number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int flag=0;
+        for(int i=1;i<n;i++){
+            if((i*(i+1))==n){
+             flag=1;
+                break;
+            }
+        }
+        if (flag==1){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
