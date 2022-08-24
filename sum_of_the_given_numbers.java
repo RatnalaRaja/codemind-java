97a9faf5f9963eb57b86adb1d0f4dd78bf44d9f1
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-08-24 20:58:21 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int t=sc.nextInt();         for(int i=1;i<t+1;i++){             int a=sc.nextInt(),b=sc.nextInt();             int s=a+b;         System.out.println(s);         }     }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<t+1;i++){
+            int a=sc.nextInt(),b=sc.nextInt();
+            int s=a+b;
+        System.out.println(s);
+        }
+    }
+    
+}
