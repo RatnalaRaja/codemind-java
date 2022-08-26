fc9d9e3c984aa34b33dbb15b2b4e235399a2147c
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-08-26 23:53:27 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         double n=sc.nextFloat();         double f=((n*9)/5)+32;         System.out.format("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextFloat();
+        double f=((n*9)/5)+32;
+        System.out.format("%.2f",f);
+    }
+}
