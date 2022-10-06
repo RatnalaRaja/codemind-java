d236705cf17314eef42337e7255b379082bed1ba
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-10-06 17:55:50 +0530

    import java.util.*; class patt{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         for(int i=n;i>=1;i--){             for(int j=n;j>=1;j--){                 System.out.print(j);                 System.out.print(" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,14 @@
 import java.util.*;
-class pattern{
+class patt{
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner (System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i+1;j++){
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=1;j--){
                 System.out.print(j);
-                //System.out.print(" ");
+                System.out.print(" ");
             }
-            System.out.print("
-");
+            System.out.println();
         }
     }
 }
