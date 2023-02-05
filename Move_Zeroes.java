3cbc3f210bf61d842f7ed924c16cd3b843a1c3c5
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2023-02-05 15:47:36 +0530

    import java.util.*; class java{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int [n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<n;i++){             if(arr[i]==0){                 c++;             }             else{                 System.out.print(arr[i]+" ");             }         }         for(int i=0;i<c;i++){             System.out.print(0+" ");         }     } }

---
+++ Move_Zeroes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class java{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                c++;
+            }
+            else{
+                System.out.print(arr[i]+" ");
+            }
+        }
+        for(int i=0;i<c;i++){
+            System.out.print(0+" ");
+        }
+    }
+}
